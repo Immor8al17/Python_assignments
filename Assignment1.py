c37da70adfae9f1aa2422f025634512d98ae64ed
Immor8al17 <149953843+Immor8al17@users.noreply.github.com>
2026-06-18 11:51:16 +0530

    Create Assignment1.py

---
+++ Assignment1.py
@@ -0,0 +1,18 @@
+
+#print statement with seperator
+print("2026", "06", "18", sep="-")
+
+
+# Print Statements with multiple Objects
+name = "Aman"
+age = 20
+print("User name is", name, "and age is", age)
+
+#print statement with ends
+
+print("Hello", end=" ")
+print("World")
+
+# Greeting user with taking user input
+user1 = input("Enter your name")
+print("Good Morning: " , user1)
